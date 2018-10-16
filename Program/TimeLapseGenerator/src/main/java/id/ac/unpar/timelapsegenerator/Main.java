bc8a7377373fac6b16977bbdb73b1d883c5cc347
billyAdi <billz.wew@gmail.com>
2018-10-16 08:27:29 +0700

    Melanjutkan program (sudah bisa membuat .gif) + Mengerjakan Bab 3 awal

--- Program/TimeLapseGenerator/src/main/java/id/ac/unpar/timelapsegenerator/Main.java
+++ Program/TimeLapseGenerator/src/main/java/id/ac/unpar/timelapsegenerator/Main.java
@@ -5,10 +5,14 @@
  */
 package id.ac.unpar.timelapsegenerator;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import javax.imageio.ImageIO;
+import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageOutputStream;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -34,6 +38,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
  * @author user
  */
 public class Main {
+
     //C:/xampp/htdocs/DNArtworks/.git
     //C:/xampp/htdocs/Piktora/.git
     public static void main(String[] args) throws IOException, GitAPIException, ParseException {
@@ -41,35 +46,35 @@ public class Main {
         Option captureURL = Option.builder().longOpt("capture-url").argName("url").hasArg().desc("link yang akan di capture").build();
         Option fps = Option.builder().longOpt("fps").argName("fps").hasArg().desc("fps video").build();
         Option beforeCapture = Option.builder().longOpt("before-capture").argName("url").hasArg().desc("migrate database").build();
-        Option projectPath=Option.builder().longOpt("project-path").argName("path").hasArg().desc("project path").build();
+        Option projectPath = Option.builder().longOpt("project-path").argName("path").hasArg().desc("project path").build();
         options.addOption(beforeCapture);
         options.addOption(captureURL);
         options.addOption(fps);
         options.addOption(projectPath);
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
-        
-        if(cmd.hasOption("fps")){
-            String temp=cmd.getOptionValue("fps");
+
+        if (cmd.hasOption("fps")) {
+            String temp = cmd.getOptionValue("fps");
             System.out.println(temp);
         }
-        if(cmd.hasOption("before-capture")){
-            String temp=cmd.getOptionValue("before-capture");
+        if (cmd.hasOption("before-capture")) {
+            String temp = cmd.getOptionValue("before-capture");
             System.out.println(temp);
         }
-        if(cmd.hasOption("capture-url")){
-            String temp=cmd.getOptionValue("capture-url");
+        if (cmd.hasOption("capture-url")) {
+            String temp = cmd.getOptionValue("capture-url");
             System.out.println(temp);
         }
-        if(cmd.hasOption("project-path")){
-            String temp=cmd.getOptionValue("project-path");
+        if (cmd.hasOption("project-path")) {
+            String temp = cmd.getOptionValue("project-path");
             System.out.println(temp);
         }
-        
+
 //        Repository repo = new FileRepository("C:\\xampp\\htdocs\\Piktora\\.git");
-          Repository repo = new FileRepository(cmd.getOptionValue("project-path"));  
+        Repository repo = new FileRepository(cmd.getOptionValue("project-path"));
 //        // System.out.println(repo.getBranch());
-//
+
         Git git = new Git(repo);
 
         RevWalk revWalk = new RevWalk(repo);
@@ -86,28 +91,46 @@ public class Main {
             //System.out.println(commit.getAuthorIdent().getName()+" "+commit.getAuthorIdent().getEmailAddress()+d.getDate()+" "+d.getMonth()+" "+d.getYear());            
             commitID.add(commit.getName().substring(0, 7));
         }
-        //driver = new ChromeDriver();
-       // driver.get(cmd.getOptionValue("before-capture"));
-        //driver.quit();
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(cmd.getOptionValue("before-capture"));
         for (int i = 0; i < commitID.size(); i++) {
-            if(i==1)continue;
+            if (i == 1) {
+                continue;
+            }
             git.checkout().setName(commitID.get(i)).call();
             if (i == 0) {
                 driver.navigate().to(cmd.getOptionValue("capture-url"));
-            }
-            else {
+            } else {
                 driver.navigate().refresh();
             }
-
+            //file gambar masih disimpan di folder hasil screenshot. Harusnya ga perlu, nanti akan ditambahkan var array of file untuk menampung image Buffered
+            
             File scrFile
                     = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            FileUtils.moveFile(scrFile, new File("hasil_screenshot/ss" + i + ".png"));
+            FileUtils.moveFile(scrFile, new File("hasil_screenshot/ss" + i + ".png")); 
 
         }
         driver.quit();
         git.checkout().setName("master").call();
+
+        BufferedImage firstImage = ImageIO.read(new File("hasil_screenshot/ss0.png")); //file pertama
+
+        
+        ImageOutputStream output
+                = new FileImageOutputStream(new File("hasil_screenshot/output.gif")); //file output
+
+        GifSequenceWriter writer= new GifSequenceWriter(output, firstImage.getType(), 500, false); //500ms adalah jarak antar frame, fpsnya= 1/0.5 s=2 fps. fps belum menggunakan nilai dari parameter
+
+        
+        writer.writeToSequence(firstImage);
+        for (int i = 2; i < commitID.size(); i++) {
+            BufferedImage nextImage = ImageIO.read(new File("hasil_screenshot/ss"+i+".png"));
+            writer.writeToSequence(nextImage);
+        }
+
+        writer.close();
+        output.close();
     }
 }

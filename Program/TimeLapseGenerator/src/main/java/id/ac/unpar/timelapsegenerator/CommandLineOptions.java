9c9b88b69e9d26bc30db8405370b8cf4089c6f31
billyAdi <billz.wew@gmail.com>
2019-02-16 04:14:58 +0700

    Menambahkan penanganan error pada program

--- Program/TimeLapseGenerator/src/main/java/id/ac/unpar/timelapsegenerator/CommandLineOptions.java
+++ Program/TimeLapseGenerator/src/main/java/id/ac/unpar/timelapsegenerator/CommandLineOptions.java
@@ -5,7 +5,7 @@
  */
 package id.ac.unpar.timelapsegenerator;
 
-import java.io.IOException;
+import java.util.Properties;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -14,25 +14,28 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 /**
- * Kelas ini berfungsi untuk menyimpan semua Option yang terdapat pada program dan melakukan parsing
- * argumen Command Line Option.
- * 
- * @author @author Billy Adiwijaya
+ * Kelas ini berfungsi untuk menyimpan semua Option yang terdapat pada program
+ * dan melakukan parsing argumen Command Line Option.
+ *
+ * @author Billy Adiwijaya
  */
 public class CommandLineOptions {
-    private CommandLine commandLine;
-    
+
+    private final CommandLine commandLine;
+
     /**
-     * Constructor dari kelas ini.
-     * Berfungsi untuk menentukan Option yang terdapat pada program dan melakukan parsing argumen Command Line. 
-     * @param args merupakan argumen Command Line Option yang didapatkan dari kelas Main.
-     * @throws ParseException jika terjadi masalah saat melakukan parsing.
+     * Constructor dari kelas ini. Berfungsi untuk menentukan Option yang
+     * terdapat pada program dan melakukan parsing argumen Command Line.
+     *
+     * @param args merupakan argumen Command Line Option yang didapatkan dari
+     * kelas Main.
+     * @throws ParseException jika terjadi masalah saat melakukan parsing atau jumlah argumen capture-url lebih dari 4.
      */
     public CommandLineOptions(String[] args) throws ParseException {
         CommandLineParser parser = new DefaultParser();
 
         Options options = new Options();
-        
+
         options.addOption(Option.builder().required().hasArgs().longOpt("capture-url").argName("url").desc("link yang akan di capture").build());
         options.addOption(Option.builder().required().longOpt("seconds-per-commit").argName("seconds").hasArg().desc("durasi satu commit").build());
         options.addOption(Option.builder().required().longOpt("project-path").argName("path").hasArg().desc("path proyek perangkat lunak").build());
@@ -43,13 +46,31 @@ public class CommandLineOptions {
         options.addOption(Option.builder().longOpt("logo").argName("image path").hasArg().desc("logo yang akan ditampilkan di pojok kanan bawah").build());
 
         this.commandLine = parser.parse(options, args);
+        if(this.commandLine.getOptionValues("capture-url").length>4){
+            throw new ParseException("Jumlah url yang akan dicapture maksimal 4");
+        }
     }
 
     /**
      * Method ini berfungsi untuk mengembalikan option yang sudah diparsing
-     * @return option yang sudah diparsing berupa array of Option
+     *
+     * @return option yang sudah diparsing berupa objek dengan tipe Properties
      */
-    public Option[] getParsedOptions() {
-        return this.commandLine.getOptions();
+    public Properties getParsedOptions() {
+        Properties properties = new Properties();
+        for (Option option : this.commandLine.getOptions()) {
+            if (option.getLongOpt().equals("capture-url")) {
+                String[] values = option.getValues();
+                String value = values[0];
+                 
+                for (int i = 1; i < values.length; i++) {
+                    value = value + ";" + values[i];
+                }
+                properties.setProperty(option.getLongOpt(), value);
+            } else {
+                properties.setProperty(option.getLongOpt(), option.getValue());
+            }
+        }
+        return properties;
     }
 }
--- Program/TimeLapseGenerator/src/main/java/id/ac/unpar/timelapsegenerator/Main.java
+++ Program/TimeLapseGenerator/src/main/java/id/ac/unpar/timelapsegenerator/Main.java
@@ -5,10 +5,9 @@
  */
 package id.ac.unpar.timelapsegenerator;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.ParseException;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 /**
@@ -16,30 +15,73 @@ import org.eclipse.jgit.api.errors.GitAPIException;
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException, ParseException, InterruptedException {
-        CommandLineOptions commandLineOptions = new CommandLineOptions(args);
-        Properties properties = new Properties();
-        int numberOfBrowsers=0;
-        for (Option option : commandLineOptions.getParsedOptions()) {
-            if (option.getLongOpt().equals("capture-url")) {
-                String[] values = option.getValues();
-                numberOfBrowsers=values.length;
-                if(numberOfBrowsers>4){
-                    numberOfBrowsers=4;
-                }
-                String value = values[0];
-                for (int i = 1; i < numberOfBrowsers; i++) {
-                    value = value + ";" + values[i];
-                }
-                properties.setProperty(option.getLongOpt(), value);
-            } else {
-                properties.setProperty(option.getLongOpt(), option.getValue());
-            }
+    public static void main(String[] args) throws GitAPIException, IOException, InterruptedException {
+        CommandLineOptions commandLineOptions = null;
+        try {
+            commandLineOptions = new CommandLineOptions(args);
+        } catch (Exception e) {
+            System.out.println("Animasi timelapse gagal dibuat");
+            System.out.println(e.getMessage());
+            System.exit(0);
         }
-        VCS vcs = new VCS(properties.getProperty("project-path"));
+        Properties properties = commandLineOptions.getParsedOptions();
+        int numberOfBrowsers = properties.getProperty("capture-url").split(";").length;
+
+        VCS vcs = null;
+        try {
+            if (!new File(properties.getProperty("project-path")).exists()) {
+                throw new Exception("Path proyek tidak valid");
+            }
+            vcs = new VCS(properties.getProperty("project-path"));
+            
+            if (Integer.parseInt(properties.getProperty("seconds-per-commit")) <= 0) {
+                throw new Exception("Seconds per commit harus lebih besar dari 0");
+            }
+            if (properties.getProperty("before-capture") != null && !new File(properties.getProperty("before-capture")).exists()) {
+                throw new Exception("Path script PHP tidak valid");
+            }
+            if (properties.getProperty("logo") != null && !new File(properties.getProperty("logo")).exists()) {
+                throw new Exception("Path gambar tidak valid");
+            }
+            if (properties.getProperty("start-commit") != null) {
+                if (properties.getProperty("start-commit").length() != 7) {
+                    throw new Exception("Panjang commit ID awal harus 7 karakter");
+                }
+                if (vcs.getCommitIndex(properties.getProperty("start-commit")) == -1) {
+                    throw new Exception("Commit ID awal tidak ditemukan");
+                }
+            }
+
+            if (properties.getProperty("stop-commit") != null) {
+                if (properties.getProperty("stop-commit").length() != 7) {
+                    throw new Exception("Panjang commit ID akhir harus 7 karakter");
+                }
+                if (vcs.getCommitIndex(properties.getProperty("stop-commit")) == -1) {
+                    throw new Exception("Commit ID akhir tidak ditemukan");
+                }
+            }
+
+            if (properties.getProperty("start-commit") != null && properties.getProperty("stop-commit") != null) {
+                if (vcs.getCommitIndex(properties.getProperty("start-commit")) > vcs.getCommitIndex(properties.getProperty("stop-commit"))) {
+                    throw new Exception("Commit ID awal dan akhir terbalik");
+                } else if (vcs.getCommitIndex(properties.getProperty("start-commit")) == vcs.getCommitIndex(properties.getProperty("stop-commit"))) {
+                    throw new Exception("Commit ID awal dan akhir tidak boleh sama");
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Animasi timelapse gagal dibuat");
+            System.out.println("Seconds per commit harus berupa bilangan bulat");
+            System.exit(0);
+        } catch (Exception e) {
+            System.out.println("Animasi timelapse gagal dibuat");
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+
         BrowserController browserController = new BrowserController(numberOfBrowsers);
         TimeLapseGenerator timeLapseGenerator = new TimeLapseGenerator();
         timeLapseGenerator.generateTimelapse(properties, vcs, browserController);
+        System.out.println("Animasi timelapse berhasil dibuat");
     }
 
 }
